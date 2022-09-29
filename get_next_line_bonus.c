9be3b2adf3ef2c9c77077e74eaadcce3b8439cb9
mide-sou <98954377+mide-sou@users.noreply.github.com>
2022-09-29 16:17:37 +0100

    final

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -14,25 +14,24 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buff[1024][BUFFER_SIZE];
+	static char	buffer[1024][BUFFER_SIZE];
 	char		*line;
 	int			i;
-	int			docsize;	
+	int			docsize;
 
 	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) == -1 || fd > 1024)
 		return (0);
 	line = NULL;
 	i = 1;
-	/* Enquanto o documento ainda não tiver chegado ao fim docsize > 0 e a linha nao tiver chegado ao fim \n */
 	while (i)
 	{
 		docsize = 1;
-		if (!buff[fd][0])
-			docsize = read(fd, buff[fd], BUFFER_SIZE);
-		i = (check_line_end(buff[fd]) && docsize > 0);
+		if (!buffer[fd][0])
+			docsize = read(fd, buffer[fd], BUFFER_SIZE);
+		i = (check_line_end(buffer[fd]) && docsize > 0);
 		if (docsize > 0)
-			line = get_line(line, buff[fd]);
-		clean_buff(buff[fd]);
+			line = get_line(line, buffer[fd]);
+		ft_clean_buff(buffer[fd]);
 	}
 	return (line);
 }
