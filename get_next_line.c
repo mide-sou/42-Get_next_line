dd6d74222beb7c5300e4f06e078502db16429384
mide-sou <98954377+mide-sou@users.noreply.github.com>
2022-09-29 16:15:58 +0100

    final

--- get_next_line.c
+++ get_next_line.c
@@ -14,10 +14,10 @@
 
 char	*get_next_line(int fd)
 {
-	static char	buff[BUFFER_SIZE];
+	static char	buffer[BUFFER_SIZE];
 	char		*line;
 	int			i;
-	int			docsize;	
+	int			docsize;
 
 	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) == -1)
 		return (0);
@@ -26,12 +26,12 @@ char	*get_next_line(int fd)
 	while (i)
 	{
 		docsize = 1;
-		if (!buff[0])
-			docsize = read(fd, buff, BUFFER_SIZE);
-		i = (check_line_end(buff) && docsize > 0);
+		if (!buffer[0])
+			docsize = read(fd, buffer, BUFFER_SIZE);
+		i = (check_line_end(buffer) && docsize > 0);
 		if (docsize > 0)
-			line = get_line(line, buff);
-		clean_buff(buff);
+			line = get_line(line, buffer);
+		ft_clean_buff(buffer);
 	}
 	return (line);
 }
